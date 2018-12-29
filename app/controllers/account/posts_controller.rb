5bdf12d5eb31f2eecba53ba2fc40ff8245a3e76b
javiel <zehua046@gmail.com>
2018-12-29 23:00:43 +0800

    implement edit and delete for account/posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -2,4 +2,29 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post Deleted."
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ApplicationRecord
   validates :title, presence: true
 
-  has_many :posts
+  has_many :posts, dependent: :destroy 
   belongs_to :user
   has_many :group_relationships
   has_many :members, through: :group_relationships, source: :user
