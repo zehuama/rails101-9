ae5d46521223056f6775033e94fe824f338fd550
javiel <zehua046@gmail.com>
2018-12-29 23:19:31 +0800

    implement edit and delete for account/groups

--- app/controllers/account/groups_controller.rb
+++ app/controllers/account/groups_controller.rb
@@ -2,4 +2,29 @@ class Account::GroupsController < ApplicationController
   def index
     @groups = current_user.participated_groups
   end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to account_groups_path, notice: "修改成功！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to account_groups_path, alert: "已删除。"
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 end
