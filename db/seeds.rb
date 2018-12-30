b7582419a72c2088f212ae6ad021434157b119b0
javiel <zehua046@gmail.com>
2018-12-30 17:36:34 +0800

    automatic create groups and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Hello World!"
+puts '这个种子档会自动建一个账号，5个groups，每个9个posts。'
+create_account = User.create([email: 'example2@gmail.com', password: '123123', password_confirmation: '123123', name: '试一试。'])
+create_groups = for i in 1..5 do
+  Group.create!([title: "Group no.#{i}", description: "这是用种子建立的第 #{i} 个讨论版", user_id: "1"])
+  GroupRelationship.create(group_id: i, user_id: 1)
+  for k in 1..8 do
+    Post.create!([group_id: "#{i}", content: "这是用种子建立的第#{k}个留言", user_id: "1"])
+  end
+end
